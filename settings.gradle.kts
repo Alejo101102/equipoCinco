41697e8e579af62a127c4ab4ffefbebe6658fa5b
Alejandro <alejogc2002@hotmail.com>
2025-12-01 23:20:59 -0500

    Commit base

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    plugins {
+        id("androidx.navigation.safeargs.kotlin") version "2.8.5"
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Miniproyecto1"
+include(":app")
